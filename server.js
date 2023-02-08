417d1c918ccadd79a28f6cd14fc57421355d14b1
Tommy-Sand <rosansandanasamy@gmail.com>
2023-02-08 11:10:37 -0700

    Added mongoose for Mongodb uses

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require('path');
 
 app.use(express.static('yoshi-react/public')); // rendering static pages
+
+//Connect to database
+mongoose.connect(process.env.ATLAS_URI);
+const database = mongoose.connection
 
 // Test Router
 const testRouter = require('./routes/test');

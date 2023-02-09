b59aeb7715b56e47af464b2f642edd094f5a3eb2
Omar <omar.niazie@gmail.com>
2023-02-09 14:18:34 -0700

    Updated copyright  year to 2023

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2013 Kezziah Camille Ayuno, Alinn Martinez, Tommy Sandanasamy, Allan Ma, Omar Niazie
+Copyright 2023 Kezziah Camille Ayuno, Alinn Martinez, Tommy Sandanasamy, Allan Ma, Omar Niazie
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

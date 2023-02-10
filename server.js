7c4d4c839f99ceeb4e57981b23aa21723f31807e
Kezziah <ayuno@ualberta.ca>
2023-02-10 09:46:38 -0700

    changed ordering for getAuthor

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const path = require('path');
 const { authAuthor, authAdmin } = require('./auth')
 
 app.use(express.static('yoshi-react/public')); // rendering static pages
-app.use(getAuthor)
 
 // HARDCODED TO ALWAYS WORK!! FIX W/ DB!
 adminToken = 12345
@@ -58,6 +57,8 @@ app.post('/login', (req, res) => {
   res.render("login/index")
 })
 // TODO: SHOULD MOVE FROM /LOGIN TO PUBLIC FEED 200 OK ON /TEST FOR EXAMPLE!
+
+app.use(getAuthor) // Checks if the author exists first then proceeds to other pages 
 
 // Some test page you see after logging in 
 app.get('/test', authAuthor, (req, res) => {

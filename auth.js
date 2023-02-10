239be983fefabe2dc98b0c6e2a3ccafab8694c25
Kezziah <ayuno@ualberta.ca>
2023-02-10 13:29:55 -0700

    changed some todo notes

--- auth.js
+++ auth.js
@@ -9,8 +9,8 @@ function authAuthor(req, res, next) {
 
 function authAdmin(isAdmin) {
     return (req, res, next) => {
-        console.log('Debug: Checking if the user has an admin token.')
-        // TODO: Query from the database the admin keys and see if the user has a valid admin key: req.author.adminToken != null
+        console.log('Debug: Checking if the admin header is true for the user.')
+        // TODO: Query from the database the admin keys and see if the user has a admin: true
         if (req.author.admin == false) {
             res.status(401) // 401 Unauthorized 
             return res.send('You are not an admin!')

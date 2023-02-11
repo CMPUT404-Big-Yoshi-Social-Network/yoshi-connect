5a7c8cabe978fd1618f7c19d46f36437c59fbf0a
Tommy-Sand <rosansandanasamy@gmail.com>
2023-02-11 01:35:13 -0700

    Completed most of authAuthor

--- auth.js
+++ auth.js
@@ -1,11 +1,32 @@
-function authAuthor(req, res, next) {
-    console.log(req.body)
-    /*If username and password match
-    *   Allow them in
-    * Else
-    *   Return to login with incorrect login
-    */
-    next()
+const crypto_js = require('crypto-js')
+
+async function authAuthor(req, res, Author) {
+    const { getAuthor } = require('./server');
+    const username = req.body.username;
+    const password = req.body.password;
+    if(!username || !password){
+        console.log("Authentication failed");
+        res.send("Login missing either username or password");
+        return;
+    }
+
+    const possible_author = await getAuthor(req, res);
+    if(!possible_author){
+        console.log("Authentication failed");
+        res.send("Login Unsuccessful");
+    }
+    const p_hashed_password = req.author.password;
+    console.log("Server's model: ", req.author);
+    console.log("Server's Hashed password: ", p_hashed_password)
+    console.log("Client's Hashed password: ", crypto_js.SHA256(password))
+    if(p_hashed_password == crypto_js.SHA256(password)){
+        console.log("Authentication successful");
+        res.send("Login successful");
+        return;
+    }
+    if(next)
+        next();
+    return;
 }
 
 function authAdmin(isAdmin) {

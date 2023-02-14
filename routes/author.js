213e00b68d7db78a536af3795eabecc1a324d84a
Kezziah <ayuno@ualberta.ca>
2023-02-14 12:09:19 -0700

    updated author register

--- routes/author.js
+++ routes/author.js
@@ -1,9 +1,12 @@
 const crypto_js = require('crypto-js');
-const { author_scheme } = require('../db_schema/author_schema.js');
+const UIDGenerator = require('uid-generator')
+const uidgen = new UIDGenerator();
+const { author_scheme, login_scheme } = require('../db_schema/author_schema.js');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const database = mongoose.connection;
 const Author = database.model('Author', author_scheme);
+const Login = database.model('Login', login_scheme);
 
 async function register_author(req, res){
     const { getAuthor } = require('../server');
@@ -14,35 +17,60 @@ async function register_author(req, res){
     }
     console.log("Debug: Author does not exist yet.")
 
-    const author_id = (await Author.find().sort({authorId:-1}).limit(1))[0].authorId + 1;
+    const authorId = (await Author.find().sort({authorId:-1}).limit(1))[0].authorId + 1;
 
     //Check to make sure username, password, and email are present
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+    const username = req.body.data.username;
+    const email = req.body.data.email;
+    const password = req.body.data.password;
     if( !username || !email || !password ){
         console.log("Debug: Did not fill in all the cells.")
-        res.send("Unsuccessful, User Registration Incomplete");
-        return;
+        return res.json({
+            message: "You are missing username or email or password.",
+            status: "Unsuccessful"
+        });
     }
 
     var author = new Author({
-        authorId: author_id,
+        authorId: authorId,
         username: username,
         password: crypto_js.SHA256(password),
         email: email,
-        about: "To be added",
-        pronouns: "None/Specified",
+        about: "...",
+        pronouns: ".../...",
         admin: false
     });
 
     author.save((err, author, next) => {
         if(err){
             console.log(err);
-            return;
+            return res.json({
+                message: "You could not be added to the database.",
+                status: "Unsuccessful"
+            });
         }
         console.log("Debug: " + author.username + " added successfully to database");
-        return;
+        let token = uidgen.generateSync();
+        let login = new Login({
+            authorId: authorId,
+            username: username,
+            token: token,
+            admin: false
+        });
+        login.save((err, login, next) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log("Debug: Login Cached.")
+            return res.json({
+                token,
+                username: username,
+                authorId: authorId,
+                admin: false,
+                status: "Successful"
+            });
+        })
     });
 }
 

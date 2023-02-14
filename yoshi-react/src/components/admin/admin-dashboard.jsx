539d2889fd341cb4d213b0f59353c9b73ed1ce3a
Kezziah <ayuno@ualberta.ca>
2023-02-13 18:02:35 -0700

    added logout to the admin dashboard

--- yoshi-react/src/components/admin/admin-dashboard.jsx
+++ yoshi-react/src/components/admin/admin-dashboard.jsx
@@ -1,8 +1,12 @@
 function AdminDashboard() {
     // TODO: CHECK IF THE USER IS ADMIN AND LOGGED IN
+    const LogOut = () => {
+        window.localStorage.setItem("token", 'undefined');
+    }
     return (
         <div>
             Hello. You are viewing the admin dashboard.
+            <a href="/login" onClick={LogOut}>Log Out</a>
         </div>
     )
 }

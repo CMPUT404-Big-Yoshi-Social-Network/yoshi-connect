66233beaaf4d10c66cf134de805f3afa8a165e19
Kezziah <ayuno@ualberta.ca>
2023-02-13 17:52:26 -0700

    path to public

--- yoshi-react/src/components/feeds/public.jsx
+++ yoshi-react/src/components/feeds/public.jsx
@@ -1,8 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
 function PublicFeed() {
+    const navigate = useNavigate();
+    const checkForAuthor = () => {
+        const token = localStorage.getItem('token');
+        if (token === 'undefined') {
+            console.log("Debug: You are not logged in.")
+            return navigate('/login');
+        }
+        console.log("Debug: You are logged in.")
+    }
+    useEffect(() => {
+        checkForAuthor();
+    });
     return (
         <div>
             Welcome to the Public Feed. You are signed in.
-        </div>
+            <a href="/login">Log Out</a>
+        </div> 
     )
 }
 

fe5fe4d84ced43e7558a49830ebb1c62210777bc
Kezziah <ayuno@ualberta.ca>
2023-02-13 18:01:17 -0700

    added protection to friendfeed

--- yoshi-react/src/components/feeds/friend.jsx
+++ yoshi-react/src/components/feeds/friend.jsx
@@ -1,4 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 function FriendFeed() {
+    const navigate = useNavigate();
+    const checkForAuthor = () => {
+        const token = localStorage.getItem('token');
+        if (token === 'undefined') {
+            console.log("Debug: You are not logged in.")
+            alert("You are not logged in. Please log in!")
+            return navigate('/login');
+        }
+        console.log("Debug: You are logged in.")
+    }
+    useEffect(() => {
+        checkForAuthor();
+    });
     return (
         <div>
             Welcome to the Friend Feed. You are signed in.

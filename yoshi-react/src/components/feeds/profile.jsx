b4c1dfccd740847eec7c883c71c4a728b416e855
Kezziah <ayuno@ualberta.ca>
2023-02-16 19:07:26 -0700

    small changes

--- yoshi-react/src/components/feeds/profile.jsx
+++ yoshi-react/src/components/feeds/profile.jsx
@@ -1,10 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from 'axios';
 function Profile() {
     const { username } = useParams();
     const navigate = useNavigate();
-    const [personal, setPersonal] = useState(null);
     const checkExpiry = () => {
         let config = {
             method: 'post',
@@ -33,12 +32,6 @@ function Profile() {
         });
     }
     useEffect(() => {
-        /*
-        let isLogged = loggedIn();
-        if (isLogged) {
-            checkExpiry();
-        }
-        */
        checkExpiry();
     });
     useEffect(() => {
@@ -51,8 +44,7 @@ function Profile() {
             axios
             .get('/' + username, config)
             .then((response) => {
-                console.log(response.status)
-                if (response.status == 404) {
+                if (response.status === 404) {
                     console.log("Debug: Profile does not exist.");
                     navigate('/notfound'); // 404 Not Found
                 } 
@@ -62,7 +54,7 @@ function Profile() {
             })
             .catch(err => {
               console.error(err);
-              if (err.response.status == 404) {
+              if (err.response.status === 404) {
                 console.log("Debug: Profile does not exist.");
                 navigate('/notfound'); // 404 Not Found
             }
@@ -71,54 +63,7 @@ function Profile() {
 
         isRealProfile();
     }, [username, navigate]);
-    /*
-    useEffect(() => {
-        const isPersonal = () => {
-            let config = {
-                method: 'get',
-                maxBodyLength: Infinity,
-                url: '/:username'
-            }
-            axios
-            .get('/:username', config)
-            .then((response) => {
-                if (response.data.personal === true) {
-                    console.log("Debug: You are viewing your own account.")
-                    setPersonal(true);
-                } else {
-                    console.log("Debug: You are viewing someone else's account.")
-                    setPersonal(false);
-                }
-            })
-            .catch(err => {
-              console.error(err);
-            });
-        }
-        isPersonal();
-    }, [setPersonal, username]);
-    
-    const LogOut = () => {
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: '/',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {
-                message: 'Logging Out'
-            }
-        }
-        axios
-        .post('/feed', config)
-        .then((response) => console.log(response))
-        .catch(err => {
-          console.error(err);
-        });
-    }
-    */
-    // Check for personal state: 
-    console.log("We are viewing our own profile, true or false? > " + personal);
+
     return (
         <div>
             You are viewing profile. Welcome to {username}'s profile!

a24d50f755367485ddb0f367d35c59c5bdf3e4eb
niazieo <omar.niazie@gmail.com>
2023-02-22 17:17:49 -0700

    a

--- yoshi-react/src/components/signup/signup.jsx
+++ yoshi-react/src/components/signup/signup.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { Form, Card, Button } from 'react-bootstrap';
+import { Form, Card, Button, Container, Image } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './signup.css';
 
@@ -76,41 +76,44 @@ export default function Signup() {
       }
     }
     return(
-      <body>
+      <>
+        <Image fluid src='/images/yoshi_connect_logo2.png' alt='Logo' width={100} />
+        <Container className='hello2'>
+            Yoshi Connect
+        </Container>
         <Card className="card">
           <Card.Header>
-              <h4>Sign Up</h4>
+              <h3>Sign Up</h3>
           </Card.Header>
           <Card.Body>
-              <Form>
-                  <Form.Group className="mb-3">
+              <Form className='form'>
+                  <Form.Group className="a">
                     <p>Username</p>
                       <Form.Control
                           name="username"
                           onChange={(e) => {setData({...data, username: e.target.value})}}
-                          type="text" placeholder="Username"/>
+                          type="text" className='box'/>
                   </Form.Group>
-                  <Form.Group className="mb-3">
+                  <Form.Group className="a">
                     <p>Email</p>
                       <Form.Control
                           name="email"
                           onChange={(e) => {setData({...data, username: e.target.value})}}
-                          type="email" placeholder="Email"/>
+                          type="email" className='box'/>
                   </Form.Group>
-                  <Form.Group className="mb-3">
+                  <Form.Group className="a">
                     <p>Password</p>
                       <Form.Control
                           name="password"
                           onChange={(e) => {setData({...data, username: e.target.value})}}
-                          type="password" placeholder="Password"/>
+                          type="password" className='box'/>
                   </Form.Group>
-                  <Form.Group className="mb-3">
-                      <Button onClick={getAccount} variant="warning" type="submit">Create Account</Button>
-                  </Form.Group>
+                  <br></br>
+                  <Button href='/' variant="warning" type="submit" className='button'>Back</Button>
+                  <Button onClick={getAccount} variant="warning" type="submit" className='button'>Next</Button>
               </Form>
           </Card.Body>
         </Card>
-      </body>
-      
+      </>
     )
   }
--- yoshi-react/src/components/welcome/welcome.jsx
+++ yoshi-react/src/components/welcome/welcome.jsx
@@ -4,14 +4,14 @@ import './welcome.css'
 
 function Welcome() {
     return (
-        <body>
+        <>
             <Image fluid src='/images/yoshi_connect_logo2.png' alt='Logo' width={100} />
             <Container className='hello'>
                 Welcome to Yoshi Connect.
             </Container>
             <Button className='button' href='/signup'>Sign Up</Button>
             <Button className='button' href='/login'>Log In</Button>
-        </body>
+        </>
     )
 }
 

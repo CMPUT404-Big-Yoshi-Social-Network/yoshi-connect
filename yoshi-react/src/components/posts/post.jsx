1dbf3742a899cf3f81749829d2b6c2479d8ec3f3
HolyHero <88dark.allan@gmail.com>
2023-02-23 18:49:27 -0700

    Hmmm

--- yoshi-react/src/components/posts/post.jsx
+++ yoshi-react/src/components/posts/post.jsx
@@ -47,52 +47,6 @@ function Post() {
         setIsOpen(!isOpen);
     }
 
-    const PostMenu = props => {
-        return (
-            <div className={"postMenuPage"}>
-                <div className={"postMenuBox"}>
-                    <button className={"postMenuCloseButton"} onClick={togglePostMenu}>x</button>
-                    <h1 className={"createPostHeader"}>Create New Post</h1>
-                    <hr size={"2px"} width={"fill"} color={"white"}/>
-                    <form>
-                        <select className={"postMenuDropDown"}  id={"format"} name={"format"} onChange={(e) => {
-                            setData({...data, format: e.target.value})
-                        }}>
-                            <option value={"plaintext"}>Plain Text</option>
-                            <option value={"markdown"}>MarkDown</option>
-                        </select>
-                        <select className={"postMenuDropDown"}  id={"scope"} name={"scope"} onChange={(e) =>{
-                            setData({...data, scope: e.target.value})
-                        }} >
-                            <option value={"Public"}>Public</option>
-                            <option value={"Friends Only"}>Friends Only</option>
-                        </select>
-                        {/*<label><p>Title</p></label>*/}
-                        {/*<input type={"text"} onChange={(e) => {*/}
-                        {/*    setData({...data, title: e.target.value})*/}
-                        {/*}}></input>*/}
-                        <label><p style={{color:"white"}}>Description</p></label>
-                        <textarea className={"postMenuInput"} id={"description"} name={"description"} rows={"8"}
-                                  wrap="physical" maxLength={"150"} onChange={(e) =>{
-                            setData({...data, content: e.target.value})
-                        }}/>
-                        <div style={{color:"white", textAlign:"right"}}>
-                            0/150
-                        </div>
-                        <label><p style={{color:"white"}}>Image Upload</p></label>
-                        <textarea className={"postMenuInput"} id={"image"} name={"impage"} rows={"8"} wrap="physical" onChange={(e) =>{
-                            setData({...data, content: e.target.value})
-                        }}/>
-                        <div style={{color:"white", textAlign:"right"}}>
-                            25MB
-                        </div>
-                        <button className={"createPostButton"} onClick={post_post}>Create Post</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
-
     return (
         <div>
             <input
@@ -100,7 +54,46 @@ function Post() {
                 value={"Create Post"}
                 onClick={togglePostMenu}
             />
-            {isOpen && <PostMenu/>}
+            {isOpen &&
+                <div className={"postMenuPage"}>
+                    <div className={"postMenuBox"}>
+                        <button className={"postMenuCloseButton"} onClick={togglePostMenu}>x</button>
+                        <h1 className={"createPostHeader"}>Create New Post</h1>
+                        <hr size={"2px"} width={"fill"} color={"white"}/>
+                        <form>
+                            <select className={"postMenuDropDown"}  id={"format"} name={"format"} onChange={(e) => {
+                                setData({...data, format: e.target.value})
+                            }}>
+                                <option value={"plaintext"}>Plain Text</option>
+                                <option value={"markdown"}>MarkDown</option>
+                            </select>
+                            <select className={"postMenuDropDown"}  id={"scope"} name={"scope"} onChange={(e) =>{
+                                setData({...data, scope: e.target.value})
+                            }} >
+                                <option value={"Public"}>Public</option>
+                                <option value={"Friends Only"}>Friends Only</option>
+                            </select>
+
+                            <label><p style={{color:"white"}}>Description</p></label>
+                            <textarea className={"postMenuInput"} id={"description"} name={"description"} rows={"8"}
+                                      wrap="physical" maxLength={"150"} onChange={(e) =>{
+                                setData({...data, content: e.target.value})
+                            }}/>
+                            <div style={{color:"white", textAlign:"right"}}>
+                                0/150 (doesn't actually count)
+                            </div>
+                            <label><p style={{color:"white"}}>Image Upload</p></label>
+                            <textarea className={"postMenuInput"} id={"image"} name={"image"} rows={"8"} wrap="physical" onChange={(e) =>{
+                                setData({...data, content: e.target.value})
+                            }}/>
+                            <div style={{color:"white", textAlign:"right"}}>
+                                25MB
+                            </div>
+                            <button className={"createPostButton"} onClick={post_post}>Create Post</button>
+                        </form>
+                    </div>
+                </div>
+            }
         </div>
     )
 }

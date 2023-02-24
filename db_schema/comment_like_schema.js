9a159e6482170868b7251d733d6f755579b24b16
Tommy-Sand <rosansandanasamy@gmail.com>
2023-02-24 08:44:26 -0700

    Switched comment and like schema to uuid

--- db_schema/comment_like_schema.js
+++ db_schema/comment_like_schema.js
@@ -3,34 +3,45 @@ const { Schema } = mongoose;
 const { author_scheme} = require('./author_schema.js');
 const { post_scheme } = require('./post_schema.js');
 
+const crypto = require('crypto');
+
 const comment_scheme = new Schema({
+    _id: {type: String, default: crypto.randomUUID},
     authorId: Number,
     comment: String,
     contentType: String,
-    published: String
+    published: String},
+    {versionKey: false
 })
 
 const comments_scheme = new Schema({
+    _id: {type: String, default: crypto.randomUUID},
     postId: ObjectID,
     num_of_comments: Number,
     comments: [comment_scheme],
-    likes: Number
+    likes: Number},
+    {versionKey: false
 })
 
 const like_scheme = new Schema({
+    _id: {type: String, default: crypto.randomUUID},
     authorId: Number,
     postId: Number,
-    commentId: Number
+    commentId: Number},
+    {versionKey: false
 })
 
 const likes_scheme = new Schema({
+    _id: {type: String, default: crypto.randomUUID},
     postId: ObjectID,
-    likes: [like_scheme]
+    likes: [like_scheme]},
+    {versionKey: false
 })
 
 module.exports = {
     comment_scheme,
     comments_scheme,
     like_scheme,
-    likes_scheme
+    likes_scheme},
+    {versionKey: false
 }

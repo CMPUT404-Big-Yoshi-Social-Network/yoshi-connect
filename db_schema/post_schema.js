85610b22b3b62b1f1846505c3077f5e8c5a175a0
Tommy-Sand <rosansandanasamy@gmail.com>
2023-02-24 08:44:49 -0700

    Added post_history_scheme and switched post_scheme to uuid

--- db_schema/post_schema.js
+++ db_schema/post_schema.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const { author_scheme} = require('../db_schema/author_schema.js');
+
+const crypto = require('crypto');
 
 const post_scheme = new Schema({
+    _id: {type: String, default: crypto.randomUUID},
     title: String,
     description: String,
     contentType: String,
     content: String,
-    authorId: Number,
     categories: [String],
     count: Number,
     comments: String,
     published: String,
     visibility: String,
-    unlisted: Boolean
+    unlisted: Boolean},
+    {versionKey: false
 });
 
+const post_history_scheme = new Schema({
+    _id: {type: String, default: crypto.randomUUID},
+    authorId: String,
+    num_posts: Number,
+    posts: [post_scheme]},
+    {versionKey: false
+})
 module.exports = {
-    post_scheme
+    post_scheme,
+    post_history_scheme
 }

ef154f67e6e8f62684eeb53f5fc7abbb3b0d53c2
Kezziah <ayuno@ualberta.ca>
2023-03-02 16:15:17 -0700

    notification component base

---
+++ yoshi-react/src/components/feeds/notifcation-box.jsx
@@ -0,0 +1,12 @@
+function Notifications() {
+    return (
+        <div>
+            <h1>Notification Box</h1>
+            {/* Friend Requests: received one or accepted 
+            Likes
+            Comments */}
+        </div>
+    )
+}
+
+export default Welcome;

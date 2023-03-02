2bab6065bab11b2c324cbfad0d2f5dfec9ad349c
alinnmartinez <alinn1@ualberta.ca>
2023-03-02 15:48:43 -0800

    setting imports and connecting test files to jsx files

---
+++ yoshi-react/src/components/__tests__/welcome/welcome.tests.js
@@ -0,0 +1,5 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import welcome from "../../welcome/welcome";
+import "@testing-library/jest-dom";
+

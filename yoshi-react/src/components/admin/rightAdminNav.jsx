aceefb882cb82a9ab6c2f7703161b424f5c727e0
niazieo <omar.niazie@gmail.com>
2023-03-02 22:42:43 -0700

    images and organization

--- yoshi-react/src/components/admin/rightAdminNav.jsx
+++ yoshi-react/src/components/admin/rightAdminNav.jsx
@@ -13,34 +13,33 @@ function RightAdminNavBar() {
                 <Nav>
                     <div className='rn-admdiv'>
                     {/* TODO: Needs to fetch username  */}
-                        <img className='rn-admUserImg' alt='rn-admUser' src='/images/icon_profile.png' width={40}/>
+                        <img className='rn-admUserImg' alt='rn-admUser' src='/images/public/icon_profile.png' width={40}/>
                         <Nav.Link className='rn-user'href="/users/:username">Username</Nav.Link> 
                     </div>
                     <div className='rn-admdiv'>
-                        <img className='rn-admDashImg' alt='rn-admDashImg' src='/images/icon_public_feed.png' width={25}/>
+                        <img className='rn-admDashImg' alt='rn-admDashImg' src='/images/admin/icon_dashboard.png' width={25}/>
                         <Nav.Link className='rn-admdash' href="/admin/dashboard">Dashboard</Nav.Link>
                     </div>
                     <div className='rn-admdiv'>
-                        <img className='rn-admAdminsImg' alt='rn-admAdminsImg' src='/images/icon_friends_feed.png' width={25}/>
+                        <img className='rn-admAdminsImg' alt='rn-admAdminsImg' src='/images/admin/icon_admins.png' width={25}/>
                         <Nav.Link className='rn-admadmins' href="/admin/admins">Admins</Nav.Link>
                     </div>
                     <div className='rn-admdiv'>
-                        <img className='rn-admUsersImg' alt='rn-admUsersImg' src='/images/icon_messages.png' width={25}/>
+                        <img className='rn-admUsersImg' alt='rn-admUsersImg' src='/images/admin/icon_users.png' width={25}/>
                         <Nav.Link className='rn-admuser' href="/admin/users">Users</Nav.Link>
                     </div>
-                    <div className='rn-admdiv'>
-                    {/* TODO: Create post */}
+                    {/* <div className='rn-admdiv'>
                         <img className='rn-admServerImg' alt='rn-admServerImg' src='/images/icon_create_post.png' width={25}/>
                         <Nav.Link className='rn-admserver'href="/admin/servers">Servers</Nav.Link>
                     </div>
                     <div className='rn-admdiv'>
                         <img className='rn-admNodesImg' alt='rn-admNodesImg' src='/images/icon_messages.png' width={25}/>
                         <Nav.Link className='rn-admnodes' href="/admin/nodes">Nodes</Nav.Link>
-                    </div>
+                    </div> */}
                 </Nav>
                 <div className='rn-admdiv'>
                     <a href='/settings'>
-                        <img className='rn-admCogImg' alt='rn-admCogImg' src='/images/icon_settings.png' width={40}/>
+                        <img className='rn-admCogImg' alt='rn-admCogImg' src='/images/public/icon_settings.png' width={40}/>
                     </a>
                     <Button href='/feed' variant="warning" type="submit" className='goto-user-dashboard'>User Dashboard</Button>
                 </div>
--- yoshi-react/src/components/feeds/rightNav.jsx
+++ yoshi-react/src/components/feeds/rightNav.jsx
@@ -15,30 +15,30 @@ function RightNavBar() {
                 <Nav>
                     <div className='rn-div'>
                     {/* TODO: Needs to fetch username  */}
-                        <img className='rn-pubUserImg' alt='rn-pubUser' src='/images/icon_profile.png' width={40}/>
+                        <img className='rn-pubUserImg' alt='rn-pubUser' src='/images/public/icon_profile.png' width={40}/>
                         <Nav.Link className='rn-user'href="/users/:username">Username</Nav.Link> 
                     </div>
                     <div className='rn-div'>
-                        <img className='rn-pubFeedImg' alt='rn-pubFeedImg' src='/images/icon_public_feed.png' width={25}/>
+                        <img className='rn-pubFeedImg' alt='rn-pubFeedImg' src='/images/public/icon_public_feed.png' width={25}/>
                         <Nav.Link className='rn-feed' href="/feed">Public Feed</Nav.Link>
                     </div>
                     <div className='rn-div'>
-                        <img className='rn-pubFriendsImg' alt='rn-pubFriend' src='/images/icon_friends_feed.png' width={25}/>
+                        <img className='rn-pubFriendsImg' alt='rn-pubFriend' src='/images/public/icon_friends_feed.png' width={25}/>
                         <Nav.Link className='rn-friends' href="/friends">Friends Feed</Nav.Link>
                     </div>
                     <div className='rn-div'>
-                        <img className='rn-pubMsgImg' alt='rn-pubMsgImg' src='/images/icon_messages.png' width={25}/>
+                        <img className='rn-pubMsgImg' alt='rn-pubMsgImg' src='/images/public/icon_messages.png' width={25}/>
                         <Nav.Link className='rn-msgs' href="/messages">Messages</Nav.Link>
                     </div>
                     <div className='rn-div'>
                     {/* TODO: Create post */}
-                        <img className='rn-pubPostImg' alt='rn-pubPostImg' src='/images/icon_create_post.png' width={25}/>
+                        <img className='rn-pubPostImg' alt='rn-pubPostImg' src='/images/public/icon_create_post.png' width={25}/>
                         <Nav.Link className='rn-post'href="/post">Create Post</Nav.Link>
                     </div>
                 </Nav>
                 <div className='rn-div'>
                     <a href='/settings'>
-                        <img className='rn-pubCogImg' alt='rn-pubCogImg' src='/images/icon_settings.png' width={25}/>
+                        <img className='rn-pubCogImg' alt='rn-pubCogImg' src='/images/public/icon_settings.png' width={25}/>
                     </a>
                 </div>
             </Container>

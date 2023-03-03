cdde5ac7810fa6feda8d8eed69b1f2553acd2bf0
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-03 10:13:15 -0700

    Added back the tests

--- yoshi-react/src/App.test.js
+++ yoshi-react/src/App.test.js
@@ -1,8 +1,7 @@
-/*import React from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App.js';
 
 test('renders learn react link', () => {
   render(<App />);
 });
-*/

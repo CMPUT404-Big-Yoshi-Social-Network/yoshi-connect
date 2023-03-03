dc0e53a27b20b3004fea7ecf23111831d8a9c6d9
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-02 23:13:08 -0700

    Fleshed out description

--- openAPI/options.js
+++ openAPI/options.js
@@ -2,10 +2,18 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Yoshi Connect'
+            title: 'Yoshi Connect - API',
+            description: 'This is the documentation for Yoshi Connect API endpoints',
+            license: {
+                'name': 'Apache 2.0',
+                'identifier': "Apache-2.0"
+            }
         }
     },
-    apis: ['./server.js']
+    apis: [
+        './server.js',
+        './openAPI/components.js'
+    ],
 };
 
 module.exports={

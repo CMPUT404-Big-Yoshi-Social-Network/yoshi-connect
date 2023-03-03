15c69d1716f4e299fccd5b41b6e1de6c191bcd03
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-03 10:08:44 -0700

    Commented out test

--- yoshi-react/src/App.test.js
+++ yoshi-react/src/App.test.js
@@ -1,7 +1,8 @@
-import React from 'react';
+/*import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App.js';
 
 test('renders learn react link', () => {
   render(<App />);
 });
+*/

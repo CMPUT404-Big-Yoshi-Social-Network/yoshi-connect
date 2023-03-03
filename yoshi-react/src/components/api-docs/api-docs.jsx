0db4a39ae83fd1ffce5d7e49c98fa8f7302d45db
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-03 13:39:28 -0700

    Done api docs

--- yoshi-react/src/components/api-docs/api-docs.jsx
+++ yoshi-react/src/components/api-docs/api-docs.jsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function ApiDocs() {
   //let html_docs = "";
@@ -9,14 +8,13 @@ export default function ApiDocs() {
   //useEffect( () => {
     //html_docs = get_docs();
   //});
-  const get_docs = (e) => {
-    e.prevent_default()
+  const style = {
+    position:'absolute',
+    top:'0',
+    left:'0',
+    height:'100%',
+    width:'100%',
+    border:'0',
   }
-
-  let docs = undefined;
-  let callRef = React.createRef();
-  useEffect(() => {
-    docs = axios.get("/server/api-docs");
-  })
-  return(<iframe src="http://localhost:8080/server/api-docs"></iframe>);
+  return(<iframe src="http://localhost:8080/server/api-docs" style={style} title="docs"></iframe>);
 }

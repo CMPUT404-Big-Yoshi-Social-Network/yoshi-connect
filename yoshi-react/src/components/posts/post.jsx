7e16e05b2904eb296e50aba282219a7ab1bb45e6
Allan <88dark.allan@gmail.com>
2023-03-03 03:40:02 -0700

    Update post.jsx

--- yoshi-react/src/components/posts/post.jsx
+++ yoshi-react/src/components/posts/post.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Comment from './comment';
@@ -7,8 +7,11 @@ import { useState } from 'react';
 function Post() {
     // const { veiwerId } = props;
     const veiwerId = "15d84277-d4ec-42b3-8e75-1278cd5654c2"
-    const postId = "5b34f5df-5d63-43f0-af78-976d324182b3"
+    const postId = "300c32ac-cebc-42f6-b3dd-2fb6f481f61c"
     const authorId = "15d84277-d4ec-42b3-8e75-1278cd5654c2"
+
+
+    axioz.get("/server/author")
 
     // const { authorId, postId } = useParams();
     const [data, setData] = useState({
@@ -17,35 +20,41 @@ function Post() {
         contentType: "type/plain",
         visibility: "Public",
         content: "",
-        likes: [{liker: "15d84277-d4ec-42b3-8e75-1278cd5654c2", _id: "5b34f5df-5d63-43f0"}],
-        comments: [{comment: "Whooooooo", commenterId: "15d84277-d4ec-42b3-8e75-1278cd5654c2"}],
+        likes: [],
+        comments: [],
         published: "",
-        unlisted: true,
-        image: ""
-    })
-    
-    axios.get("/server/authors/" + authorId + "/" + postId)
-    .then((response) => {
-        data.title = response.data.title;
-        data.desc = response.data.desc;
-        data.contentType = response.data.contentType;
-        data.visibility = response.data.visibility;
-        data.content = response.data.content;
-        data.likes = response.data.likes;
-        data.comments = response.data.comments;
-        data.published = response.data.published;
-        data.unlisted = response.data.unlisted;
-        data.image = response.data.image;
-    })
-    .catch((error) => { console.log(error); });
-    const listed = data.unlisted;
+        listed: false,
+        image: "",
+        liked: false
+    });
+    useEffect(() => {
+        axios.get("server/authors/" + authorId + "/posts/" + postId)
+        .then((response) => {
+            console.log(response.data[0].posts);
+            
+            data.title = response.data[0].posts.title;
+            data.desc = response.data[0].posts.desc;
+            data.contentType = response.data[0].posts.contentType;
+            data.visibility = response.data[0].posts.visibility;
+            data.content = response.data[0].posts.content;
+            response.data[0].posts.likes.forEach = (likes) => {
+                data.likes.push(likes);
+                if (likes.likers === veiwerId) { data.liked = true; }
+            };
+            // response.data[0].posts.comments.forEach = (coms) => { data.likes.push(coms); }
+            data.published = response.data[0].posts.published;
+            data.listed = response.data[0].posts.unlisted;
+            // data.image = response.data[0].posts.data.image;
+        })
+        .catch((error) => { console.log(error); });
+    });
 
-    const blobToImage = (blob) => {
-        const imageUrl = URL.createObjectURL(blob);
-        const img = document.querySelector('img');
-        img.addEventListener("load", () => URL.revokeObjectURL(imageUrl));
-        document.querySelector("img").src = imageUrl;
-    }
+    // const blobToImage = (blob) => {
+    //     const imageUrl = URL.createObjectURL(blob);
+    //     const img = document.querySelector('img');
+    //     img.addEventListener("load", () => URL.revokeObjectURL(imageUrl));
+    //     document.querySelector("img").src = imageUrl;
+    // }
 
     const [isComment, setIsComment] = useState(false)
 
@@ -61,7 +70,7 @@ function Post() {
             method: "put",
             maxBodyLength: "Infinity",
             url: "/server/authors/"+authorId+"/posts/"+postId,
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            headers: { 'Content-Type': 'application/json' },
             data: {
                 liker: veiwerId,
                 authorId: authorId,
@@ -69,8 +78,8 @@ function Post() {
                 status: "Add like"
             }
         };
-        console.log(config);
         axios.put("/server/authors/"+authorId+"/posts/"+postId, config).then((response) => {}).catch((error) => { console.log(error); });
+        data.liked = true;
     }
     const removeLike = () => {
         console.log("Removing Like");
@@ -87,6 +96,7 @@ function Post() {
             }
         };
         axios.delete("/server/authors/"+authorId+"/posts/"+postId, config).then((response) => {}).catch((error) => { console.log(error); });
+        data.liked = false;
     }
     const [comment, setComment] = useState({
         newComment: ""
@@ -111,10 +121,12 @@ function Post() {
         axios(config).then((response) => {}).catch((error) => { console.log(error); });
     }
 
+    // getData();
+    
     return (
         <div style={{backgroundColor: "grey"}}>
             <hr size={"2px"} width={"fill"} color={"black"}/>
-            {listed &&
+            {data.listed &&
                 <div>
                     {data.title === "" ? null : <h1>{data.title}</h1>}
                     <button>IDK</button>
@@ -125,7 +137,7 @@ function Post() {
                         <p>{data.content}</p> : <p>Markdown:{data.content}</p>}
                     {/* {data.image === ""? null : <img id={"image"} src={blobToImage(data.image)} alt={data.title}/>} */}
                     <p>{data.published}</p>
-                    {true ? <button onClick={removeLike}>Unlike Post</button> : <button onClick={addLike}>Like Post</button>} 
+                    {data.liked ? <button onClick={removeLike}>Unlike Post</button> : <button onClick={addLike}>Like Post</button>} 
 
                     {isComment ? <button onClick={toggleComments}>Close Comments</button> : <button onClick={toggleComments}>Open Comments</button>}
                     {isComment && 

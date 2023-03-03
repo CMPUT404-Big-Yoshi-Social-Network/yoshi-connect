2337b74cc13942d73d97c6ee026b26e9b251404a
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-03 08:59:55 -0700

    Fixing CI

--- yoshi-react/jest.config.js
+++ yoshi-react/jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('jest').Config} */
 const config = {
     extensionsToTreatAsEsm: ['.jsx'],
-  };
+    moduleNameMapper: {
+      '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    },
+};
   
   export default config;

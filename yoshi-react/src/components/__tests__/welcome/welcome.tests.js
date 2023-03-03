3585ab6de2a2d845be7dd9c4a631a61a4a4eaf96
alinnmartinez <alinn1@ualberta.ca>
2023-03-02 16:56:30 -0800

    welcome screen unit test

--- yoshi-react/src/components/__tests__/welcome/welcome.tests.js
+++ yoshi-react/src/components/__tests__/welcome/welcome.tests.js
@@ -6,17 +6,17 @@ import "@testing-library/jest-dom";
 import Counter from "../components/Counter";
 
 //test block
-test("should click on sign up button", () => {
+test("on inital render, the sign up button is there", () => {
 // render the component on virtual dom
 render(<welcome />);
 
 //select the elements you want to interact with
 const signupButton = screen.getByTestId("signup");
-const incrementBtn = screen.getByTestId("increment");
+// const incrementBtn = screen.getByTestId("increment");
 
 //interact with those elements
 fireEvent.click(signupButton);
 
 //assert the expected result
-expect(signupButton).toHaveTextContent("1");
+expect(signupButton).toBeEnabled();
 });

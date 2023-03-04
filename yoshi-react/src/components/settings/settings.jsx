f943064d17bfe866d1988b735001d0145f6b7299
alinnmartinez <alinn1@ualberta.ca>
2023-03-03 21:13:25 -0800

    imports

--- yoshi-react/src/components/settings/settings.jsx
+++ yoshi-react/src/components/settings/settings.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import React, { useEffect } from "react";
+import './settings.css';
 function Settings() {
     const navigate = useNavigate();
     const loggedIn = () => {

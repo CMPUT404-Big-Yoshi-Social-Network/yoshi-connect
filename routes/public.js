2d2c906446671746f8f8b0d911b1733d06dd8d6f
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-04 17:09:18 -0700

    Posts on the feed are now sorted by date

--- routes/public.js
+++ routes/public.js
@@ -38,14 +38,80 @@ async function fetchPublicPosts(req, res) {
     console.log('Debug: Retrieving current author logged in')
     const username = login.username
 
+    /*
     let followings = [];
-    const following = await Following.findOne({username: username}).clone()
+    const following = await Following.findOne({username: username}, 'followings').clone()
     if (following != undefined) {
         if (following.followings != [] && following.followings != null) {
             followings = following.followings
         }
     }
+    */
+
+    const following = await Following.aggregate([
+        {
+            $match: {'username': username} 
+        },
+        {
+            $unwind: '$followings'
+        },
+        {
+            $project: {
+                "followings.authorId": 1
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                follows: { $addToSet: "$followings.authorId"}
+            }
+        },
+    ]);
+
+    let followings = undefined
+    if(following.length > 0){
+        followings = following[0].follows;
+    }
+    console.log(followings);
+
     
+    if(followings == undefined){
+        return res.sendStatus(404);
+    }
+
+    const posts = await PostHistory.aggregate([
+        {
+            $match: {
+                $expr: {
+                    $in : ["$authorId", followings]
+                }
+            },
+        },
+        {
+            $unwind: "$posts"
+        },
+        {
+            $set: {
+                "posts.published": {
+                    $dateFromString: {
+                        dateString: "$posts.published"
+                    }
+                }
+            }
+        },
+        {
+            $sort: {"posts.published": -1}
+        },
+        {
+            $group: {
+                _id: null,
+                posts_array: {$push: "$posts"}
+            }
+        },
+    ]);
+
+    console.log(posts[0].posts_array);
+    /*
     let publicPosts = [];
     if (followings != undefined) {
         for (let i = 0; i < followings.length; i++) {
@@ -59,10 +125,11 @@ async function fetchPublicPosts(req, res) {
             }
         }
     }
+    */
 
     // TODO: Getting the PSA (Public Posts): Require to iterate through all the authors in order to get their posts array which indicates visibility
     return res.json({
-        publicPosts: publicPosts
+        publicPosts: []
     });
 }
 

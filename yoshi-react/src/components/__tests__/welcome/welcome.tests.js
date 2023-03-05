4adbae4106a2701fe08d17404964509347ee1654
alinnmartinez <alinn1@ualberta.ca>
2023-03-05 14:42:52 -0800

    fixing and testing the tests

--- yoshi-react/src/components/__tests__/welcome/welcome.tests.js
+++ yoshi-react/src/components/__tests__/welcome/welcome.tests.js
@@ -1,12 +1,12 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import renderer from "react-test-renderer";
-import welcome from "../../welcome/welcome";
+import Welcome from "../../welcome/welcome";
 import "@testing-library/jest-dom";
 
 //test block
 test("on inital render, the sign up button is there", () => {
 // render the component on virtual dom
-render(<welcome />);
+render(<Welcome />);
 
 //select the elements you want to interact with
 const signupButton = screen.getByTestId("signup");

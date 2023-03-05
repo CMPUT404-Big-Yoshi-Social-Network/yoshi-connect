6b4b8aff54e1410c3f0a8735a12eb0d958fb64ff
Kezziah <ayuno@ualberta.ca>
2023-03-04 17:17:56 -0700

    updated id for functions

--- routes/post.js
+++ routes/post.js
@@ -76,7 +76,6 @@ async function addComment(req, res){
     var comment = new Comment({
         type: 'comment',
         author: commenter,
-        _id: '', // http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c
         comment: req.body.data.comment,
         contentType: '', // TODO: GET CONTENT TYPE
         published: '' // TODO: GET PUBLISHED 
@@ -139,7 +138,6 @@ async function create_post(req, res, postId){
     console.log('Debug: Creating a post')
 
     // Post Attributes
-    const id = '' // TODO: "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e"
     const title = req.body.data.title;
     const source = req.body.data.source
     const origin = req.body.data.origin
@@ -166,7 +164,6 @@ async function create_post(req, res, postId){
     if (postId == undefined) {
         const new_post = new Post({
             type: 'post',
-            _id: id, // "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e"
             title: title,
             description: desc,
             source: source,

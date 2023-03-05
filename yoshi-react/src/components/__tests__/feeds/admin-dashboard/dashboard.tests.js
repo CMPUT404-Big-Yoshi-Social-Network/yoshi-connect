48d088144fc2befed1c5c98d6ed421035435e263
alinnmartinez <alinn1@ualberta.ca>
2023-03-05 15:58:15 -0800

    adding testing blocks to unit tests

--- yoshi-react/src/components/__tests__/feeds/admin-dashboard/dashboard.tests.js
+++ yoshi-react/src/components/__tests__/feeds/admin-dashboard/dashboard.tests.js
@@ -2,3 +2,10 @@ import { render, screen, cleanup } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import AdminDashboard from "../../../admin/adminDashboard";
 import "@testing-library/jest-dom";
+
+//test block
+test("test description here", () => {
+    // render the component on virtual dom
+    render(<AdminDashboard />); 
+
+});

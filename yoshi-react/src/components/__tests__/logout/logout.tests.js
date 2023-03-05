e6e7054b8b52e91b5d3badfaa6d8d4bbee8a65cb
alinnmartinez <alinn1@ualberta.ca>
2023-03-05 15:21:30 -0800

    removed log out unit test

--- yoshi-react/src/components/__tests__/logout/logout.tests.js
+++
@@ -1,4 +0,0 @@
-import { render, screen, cleanup } from "@testing-library/react";
-import renderer from "react-test-renderer";
-// import Logout from "../logout";
-import "@testing-library/jest-dom";

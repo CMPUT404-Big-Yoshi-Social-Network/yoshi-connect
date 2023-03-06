6cbc69a44813e994a6581b569744921d317f2372
Kezziah <ayuno@ualberta.ca>
2023-03-06 03:39:36 -0700

    ahh

--- yoshi-react/src/components/feeds/public/public.jsx
+++ yoshi-react/src/components/feeds/public/public.jsx
@@ -53,6 +53,35 @@ function PublicFeed() {
     const [publicPosts, setPublicPosts] = useState([]);
     const [viewer, setViewerId] = useState({ viewerId: '' })
 
+    const logOut = useCallback(() => {
+        /**
+         * Description: Sends a POST request in order to log out an author 
+         * Request: POST
+         * Returns: N/A
+         */
+        console.log('Debug: Attempting to log out.')
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: '/server/feed',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+                message: 'Logging Out'
+            }
+        }
+
+        axios
+        .post('/server/feed', config)
+        .then((response) => {
+            localStorage['sessionId'] = "";
+            navigate("/");
+        })
+        .catch(err => { console.error(err); });
+    }, [navigate]);
+
     useEffect(() => {
         /**
          * Description: Calls checkExpiry() to check if the token is expired before render 
@@ -150,35 +179,6 @@ function PublicFeed() {
         getId();
         getPosts();
     }, []);
-
-    const logOut = useCallback(() => {
-        /**
-         * Description: Sends a POST request in order to log out an author 
-         * Request: POST
-         * Returns: N/A
-         */
-        console.log('Debug: Attempting to log out.')
-
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: '/server/feed',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {
-                message: 'Logging Out'
-            }
-        }
-
-        axios
-        .post('/server/feed', config)
-        .then((response) => {
-            localStorage['sessionId'] = "";
-            navigate("/");
-        })
-        .catch(err => { console.error(err); });
-    }, [navigate]);
 
     return (
         <div>

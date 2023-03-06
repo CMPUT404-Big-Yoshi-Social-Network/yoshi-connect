989be2e3084b56c8596e136091f3746444118a00
alinnmartinez <alinn1@ualberta.ca>
2023-03-05 16:02:09 -0800

    changed lib imports + added more testing blocks

--- yoshi-react/src/components/__tests__/login/login.tests.js
+++ yoshi-react/src/components/__tests__/login/login.tests.js
@@ -1,4 +1,11 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import Login from "../../login/login";
 import "@testing-library/jest-dom";
+
+//test block
+test("test description here", () => {
+    // render the component on virtual dom
+    render(<Login />); 
+
+});
--- yoshi-react/src/components/__tests__/signup/signup.tests.js
+++ yoshi-react/src/components/__tests__/signup/signup.tests.js
@@ -1,4 +1,11 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import Signup from "../../signup/signup";
 import "@testing-library/jest-dom";
+
+//test block
+test("test description here", () => {
+    // render the component on virtual dom
+    render(<Signup />); 
+
+});

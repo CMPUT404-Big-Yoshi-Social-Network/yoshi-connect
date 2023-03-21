c2d42de3dc8763723ee1e31f388d87aa9cc73d7f
Kezziah <ayuno@ualberta.ca>
2023-03-20 22:16:20 -0600

    typo

--- yoshi-react/src/components/admin/author/author.jsx
+++ yoshi-react/src/components/admin/author/author.jsx
@@ -75,9 +75,9 @@ function Author(props) {
     
     return (
         <div className='author-div' id='author'>
-            { props.displayName === undefined ? null : 
+            { props.displayname === undefined ? null : 
                 <div>
-                    { props.displayName }
+                    { props.displayname }
                     <div className='author-button-div'>
                         <button className='author-buttons' type="button" id='delete' onClick={() => deleteAuthor()}>Delete</button>
                         <button className='author-buttons' type="button" id='enable' onClick={() => allowAuthor()}>Enable</button>

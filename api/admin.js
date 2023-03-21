58e159429fc5c196703613473225d69ef2ed5615
alinnmartinez <alinn1@ualberta.ca>
2023-03-20 18:48:46 -0700

    admin.js openapi-docs -- need to finish first and last documentation

--- api/admin.js
+++ api/admin.js
@@ -39,12 +39,24 @@ const router = express.Router({mergeParams: true});
 
 /**
  * @openapi
- * /admin:
+ * /admin: 
  *  post:
  *    description: Authenticates an potential Author for YoshiConnect
  *    responses:
- *      <INSERT>:
- *        description: <INSERT>
+ *      400:
+ *        description: Bad Request -- Invalid username or password
+ *      400:
+ *        description: Bad Request -- Author was not found in the database
+ *      500:
+ *        description: Internal Server Error -- deleting login token encountered a server-side error 
+ *      403:
+ *        description: Forbidden -- Not admin author was denied access
+ *      200:
+ *        description: OK -- Login as author was successful and cached
+ *      500:
+ *        description: Internal Server Error -- 
+ *      500:
+ *        description: Internal Server Error --
  */
 router.post('/', async (req, res) => { await authAuthor(req, res); })
 
@@ -54,8 +66,12 @@ router.post('/', async (req, res) => { await authAuthor(req, res); })
  *  get:
  *    description: Verifies Author has attribute admin=true then redirects the Author to the Admin Dashboard if verified
  *    responses:
- *      <INSERT>:
- *        description: <INSERT>
+ *      403:
+ *        description: Forbidden -- If admin is not authenticated, access is not granted
+ *      401:
+ *        description: Unauthorized -- Admin token expired and was not granted authorizatiin 
+ *      200:
+ *        description: OK -- Admin was successfully authenticated
  */
 router.get('/dashboard', async (req, res) => {
   if(!(await checkAdmin(req, res))){ return res.sendStatus(403) }
@@ -69,8 +85,12 @@ router.get('/dashboard', async (req, res) => {
  *  post:
  *    description: Removes the Login document (associated token) to log out the Admin 
  *    responses:
- *      <INSERT>:
- *        description: <INSERT>
+ *      500:
+ *        description: Internal Server Error -- Logining in with token failed server-side
+ *      200:
+ *        description: OK -- Login token was successfully deleted 
+ *      401:
+ *        description: Unauthorized -- Token is undefined, login token never existed and user access is unauthorized  
  */
 router.post('/dashboard', async (req, res) => { removeLogin(req, res); })
 
@@ -80,8 +100,12 @@ router.post('/dashboard', async (req, res) => { removeLogin(req, res); })
  *  delete:
  *    description: Deletes an Author from YoshiConnect database 
  *    responses:
- *      <INSERT>:
- *        description: <INSERT>
+ *      404:
+ *        description: Not Found -- Author was not found
+ *      500:
+ *        description: Internal Server Error -- Loging author in failed server-side
+ *      204:
+ *        description: No Content -- No author data found, author was successfully deleted
  */
 router.delete('/dashboard', (req, res) => { deleteAuthor(req, res); })
 
@@ -90,6 +114,8 @@ router.delete('/dashboard', (req, res) => { deleteAuthor(req, res); })
  * /admin/dashboard:
  *  put:
  *    description: Either Adds an Author to YoshiConnect database or Updates an Author's attributes 
+ *      - ADD: Requests an add smth to add author to database
+ *      - MODIFY: Requests a modify smth to modify existing author in database
  *    responses:
  *      <INSERT>:
  *        description: <INSERT>

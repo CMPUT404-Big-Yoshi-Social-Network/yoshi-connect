38d2c3a204f25567ae859df9094fcb5d1e196e68
Kezziah <ayuno@ualberta.ca>
2023-03-25 17:56:05 -0600

    ahh why does the schema not work

--- api/admin.js
+++ api/admin.js
@@ -120,7 +120,7 @@ router.delete('/dashboard', (req, res) => { deleteAuthor(req, res); })
  *      - admin
  *    requestBody: 
  *      content:
- *        application/x-www-form-urlencoded:
+ *        application/x-wwwm-form-urlencoded:
  *          schema:
  *            oneOf:
  *              - $ref: '#/components/schemas/NewAuthor'

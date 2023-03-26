fd572fa5e00acc33d6d6ec8658d7800194e43581
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-25 20:27:16 -0600

    Use cors

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.set('views', path.resolve( __dirname, './yoshi-react/build'));
 
 // Routing

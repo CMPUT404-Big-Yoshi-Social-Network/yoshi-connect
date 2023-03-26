5d0c394d01069ca2d79de6d5b039e2c8039d11db
Kezziah <ayuno@ualberta.ca>
2023-03-25 21:57:10 -0600

    friends docs done

--- api/friend.js
+++ api/friend.js
@@ -47,11 +47,52 @@ const router = express.Router({mergeParams: true});
  *    description: Fetches friends list associated with authorId 
  *    tags:
  *      - friend
+ *    parameters:
+ *      - in: path
+ *        name: authorId
+ *        schema:
+ *          type: string
+ *        description: id of an Author
  *    responses:
  *      404:
- *        description: Not Found -- no friends list associated with authorId 
+ *        description: Not Found, no friends list associated with authorId 
  *      200: 
- *        description: OK -- fetches and sanitizes friends list successfully 
+ *        description: OK, fetches and sanitizes friends list successfully 
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                type:
+ *                  type: string
+ *                  description: JSON type 
+ *                  example: friends
+ *                items: 
+ *                  type: array
+ *                  items: 
+ *                    type: object
+ *                  description: array of friends
+ *                  example: 
+ *                    - type: author
+ *                      id: https://yoshi-connect.herokuapp.com/authors/29c546d45f564a27871838825e3dbecb
+ *                      authorId: 29c546d45f564a27871838825e3dbecb
+ *                      host: https://yoshi-connect.herokuapp.com/
+ *                      displayName: kc
+ *                      url: https://yoshi-connect.herokuapp.com/authors/29c546d45f564a27871838825e3dbecb
+ *                      github: https://github.com/kezzayuno
+ *                      profileImage: data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAkIAAADIhkjhaDjkdHfkaSd
+ *                      about: i am a code monkey
+ *                      pronouns: she/her
+ *                    - type: author
+ *                      id: https://yoshi-connect.herokuapp.com/authors/3ec2a2a0685445509a3ea1dd3093639f
+ *                      authorId: 3ec2a2a0685445509a3ea1dd3093639f
+ *                      host: https://yoshi-connect.herokuapp.com/
+ *                      displayName: allan
+ *                      url: https://yoshi-connect.herokuapp.com/authors/3ec2a2a0685445509a3ea1dd3093639f
+ *                      github: https://github.com/Holy-Hero
+ *                      profileImage: data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAkIAAADIhkjhaDjkdHfkaSd
+ *                      about: i love hatsune miku
+ *                      pronouns: he/him
  */
 router.get('/', async (req, res) => {
   const authorId = req.params.authorId;
@@ -96,11 +137,56 @@ router.get('/', async (req, res) => {
  *    summary: Checks if the Author associated with foreignId is true friends with Author associated with authorId 
  *    tags:
  *      - friend
+ *    parameters:
+ *      - in: path
+ *        name: authorId
+ *        schema:
+ *          type: string
+ *        description: id of an Author
+ *      - in: path
+ *        name: foreignAuthorId
+ *        schema:
+ *          type: string
+ *        description: id of an foreign Author
  *    responses:
  *      401:
- *        description: Unauthorized -- no associated cookies or Login token expired 
+ *        description: Unauthorized, no associated cookies or Login token expired 
  *      200: 
- *        description: OK -- returns JSON of sanitized "relation" object which as an associated status stating their relation, "Friends", "Strangers", "Follows
+ *        description: OK, successfully checks the type of relation the foreign author has with author
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                type:
+ *                  type: string
+ *                  description: JSON type 
+ *                  example: relation
+ *                aId: 
+ *                  type: string
+ *                  description: actor url 
+ *                  example: https://yoshi-connect.herokuapp.com/authors/f08d2d6579d5452ab282512d8cdd10d4
+ *                actorId: 
+ *                  type: string
+ *                  description: UUID actor id
+ *                  example: f08d2d6579d5452ab282512d8cdd10d4
+ *                host: 
+ *                  type: string
+ *                  description: current host
+ *                  example: https://yoshi-connect.herokuapp.com/
+ *                oId: 
+ *                  type: string
+ *                  description: object url
+ *                  example: https://yoshi-connect.herokuapp.com/authors/f25cd371afbb4775930fefa6ad8828c4
+ *                objectId:
+ *                  type: string
+ *                  description: UUID object id
+ *                  example: f25cd371afbb4775930fefa6ad8828c4
+ *                status:
+ *                  type: string
+ *                  description: type of relationships (Friends, Follows, Strangers)
+ *                  example: Friends
+ *        
  */
 router.post('/:foreignId', async (req, res) => {
   if (!req.cookies || await checkExpiry(req.cookies.token)) { return res.sendStatus(401) }

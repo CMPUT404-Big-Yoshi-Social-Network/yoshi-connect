9aea5ef6d955ceb9c4bfa4a4de246c023dc58c26
Kezziah <ayuno@ualberta.ca>
2023-03-25 22:08:00 -0600

    settings docs done

--- api/settings.js
+++ api/settings.js
@@ -25,11 +25,11 @@ const router = express.Router({mergeParams: true});
  *      - settings
  *    responses:
  *      500:
- *        description: Internal Server Error -- Unable to delete login token from database
+ *        description: Internal Server Error, Unable to delete login token from database
  *      200:
- *        description: OK -- Login token was succesfully removed from database
+ *        description: OK, Login token was succesfully removed from database
  *      401:
- *        description: Unauthorized -- Login token is not authenticated
+ *        description: Unauthorized, Login token is not authenticated
  */
 router.post('/logout', async (req, res) => { removeLogin(req, res); })
 
@@ -42,11 +42,11 @@ router.post('/logout', async (req, res) => { removeLogin(req, res); })
  *      - settings
  *    responses:
  *      401:
- *        description: Unauthourized -- Author's token is not authenticated 
+ *        description: Unauthourized, Author's token is not authenticated 
  *      404:
- *        description: Not Found -- Author was not found
+ *        description: Not Found, Author was not found
  *      200:
- *        description: OK -- Returns the sanitized Author
+ *        description: OK, Returns the sanitized Author
  */
 router.put('/', async (req, res) => {
   const status = await updateAuthor(req.cookies.token, req.body.data);

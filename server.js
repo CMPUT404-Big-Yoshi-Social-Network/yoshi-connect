de25dfcc4df8c5f1c420bb049b86a87656738ec6
Kezziah <ayuno@ualberta.ca>
2023-03-28 01:54:03 -0600

    typo

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const express = require('express');
 const app = express();
 let cors = require('cors');
 let corsOptions = {
-  origin: ["http://www.distribution.social", "https://www.distribution.social/", "http://sociallydistributed.herokuapp.com", "https://sociallydistributed.herokuapp.com", "http://localhost:8000/", "http://127.0.0.1:8000/", "http://0.0.0.0:8000/"],
+  origin: ["http://www.distribution.social", "https://www.distribution.social", "http://sociallydistributed.herokuapp.com", "https://sociallydistributed.herokuapp.com", "http://localhost:8000/", "http://127.0.0.1:8000/", "http://0.0.0.0:8000/"],
   credentials: true
 };
 const PORT = process.env.PORT || 8080;

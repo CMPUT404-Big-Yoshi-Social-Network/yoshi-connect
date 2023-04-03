7d4e9cbaf3f7eb9158a79669bc5e0816a4153b14
Allan <88dark.allan@gmail.com>
2023-04-02 19:32:19 -0600

    adjusted image

--- yoshi-react/src/components/posts/post.jsx
+++ yoshi-react/src/components/posts/post.jsx
@@ -225,7 +225,7 @@ function Post({viewerId, post, author}) {
                     { post.title === "" ? null : <h1>{post.title}</h1> }
                     { post.description === "" ? null : <h3>{ post.description }</h3> }
                     { post.contentType === "text/plain" ? <p>{ post.content }</p> : post.contentType === "text/markdown" ? <ReactCommonmark source={post.content}/> : null }
-                    { image === "" ? null : <a href={"/authors/" + authorId + "/posts/" + postId + "/image"} target="_blank" rel="noreferrer" ><img className={"image"} src={image} alt=""/></a>}
+                    { image === "" ? null : <a href={image} target="_blank" rel="noreferrer" ><img className={"image"} src={image} alt=""/></a>}
 
                     <p>{published}</p>
                     <br></br>

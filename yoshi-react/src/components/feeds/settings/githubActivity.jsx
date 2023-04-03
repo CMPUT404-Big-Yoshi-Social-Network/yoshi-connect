bdc07c6ab3c001280ab24f6f2fa55439d9d5330a
Allan <88dark.allan@gmail.com>
2023-04-02 18:33:04 -0600

    Added example activity

--- yoshi-react/src/components/feeds/settings/githubActivity.jsx
+++ yoshi-react/src/components/feeds/settings/githubActivity.jsx
@@ -18,6 +18,67 @@ Foundation; All Rights Reserved
 import React from 'react';
 
 
+/*
+Example of activity
+ {
+    "id": "28150601058",
+    "type": "PushEvent",
+    "actor": {
+      "id": 47871461,
+      "login": "Holy-Hero",
+      "display_login": "Holy-Hero",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/Holy-Hero",
+      "avatar_url": "https://avatars.githubusercontent.com/u/47871461?"
+    },
+    "repo": {
+      "id": 586055695,
+      "name": "CMPUT404-Big-Yoshi-Social-Network/yoshi-connect",
+      "url": "https://api.github.com/repos/CMPUT404-Big-Yoshi-Social-Network/yoshi-connect"
+    },
+    "payload": {
+      "repository_id": 586055695,
+      "push_id": 13163434259,
+      "size": 2,
+      "distinct_size": 2,
+      "ref": "refs/heads/development",
+      "head": "5d0caa3a3d60eb74b33bcabd2bf86801d3bbc735",
+      "before": "c53de0bfde6a8ea9b88f3afa5b009125e1515bcf",
+      "commits": [
+        {
+          "sha": "b296115a96515f8992d8cbdaa03ef4bb9417b501",
+          "author": {
+            "email": "88dark.allan@gmail.com",
+            "name": "Allan"
+          },
+          "message": "Fixed status issue for updating profile",
+          "distinct": true,
+          "url": "https://api.github.com/repos/CMPUT404-Big-Yoshi-Social-Network/yoshi-connect/commits/b296115a96515f8992d8cbdaa03ef4bb9417b501"
+        },
+        {
+          "sha": "5d0caa3a3d60eb74b33bcabd2bf86801d3bbc735",
+          "author": {
+            "email": "88dark.allan@gmail.com",
+            "name": "Allan"
+          },
+          "message": "Merge branch 'development' of https://github.com/CMPUT404-Big-Yoshi-Social-Network/yoshi-connect into development",
+          "distinct": true,
+          "url": "https://api.github.com/repos/CMPUT404-Big-Yoshi-Social-Network/yoshi-connect/commits/5d0caa3a3d60eb74b33bcabd2bf86801d3bbc735"
+        }
+      ]
+    },
+    "public": true,
+    "created_at": "2023-04-03T00:27:04Z",
+    "org": {
+      "id": 122124281,
+      "login": "CMPUT404-Big-Yoshi-Social-Network",
+      "gravatar_id": "",
+      "url": "https://api.github.com/orgs/CMPUT404-Big-Yoshi-Social-Network",
+      "avatar_url": "https://avatars.githubusercontent.com/u/122124281?"
+    }
+  }
+  */
+
 
 function Activity({activity}) {
 

d43de717e44d5e9c8a900766539509eccd5007c7
Kezziah <ayuno@ualberta.ca>
2023-04-03 18:16:24 -0600

    style: msgnav fix

--- yoshi-react/src/components/feeds/messages/messagesNav.jsx
+++ yoshi-react/src/components/feeds/messages/messagesNav.jsx
@@ -36,7 +36,7 @@ function MessagesNav({authorId, messengers, currentMessenger, setCurrentMessenge
             { messengers === undefined || messengers.length === 0 ? null :
                 <div>
                     {Object.keys(messengers).map((messenger, idx) => (
-                        <Button className="search-button" key={idx} onClick={() => selectedMessenger(messengers[messenger])}><Messenger className="search-button" key={idx} authorId={authorId} messenger={messengers[messenger]} currentMessenger={currentMessenger}/></Button>
+                        <Button style={{margin:'.5em'}} key={idx} onClick={() => selectedMessenger(messengers[messenger])}><Messenger className="search-button" key={idx} authorId={authorId} messenger={messengers[messenger]} currentMessenger={currentMessenger}/></Button>
                     ))}
                 </div>
             }

63466624e40cc8468541ebca59f23ed4aefef781
Allan <88dark.allan@gmail.com>
2023-04-05 20:44:11 -0600

    Image fix

--- yoshi-react/src/components/feeds/settings/settings.jsx
+++ yoshi-react/src/components/feeds/settings/settings.jsx
@@ -145,7 +145,7 @@ function Settings() {
                 <div className='pubColM'>
                     <div className='settingColM'>
                         <div className='profile-heading'>
-                        { newAuthor.image === "" || newAuthor.image === null ? <img className='ad-pubUserImg' alt='ad-pubUser' src='/images/public/icon_profile.png' width={40}/> : <img className='ad-pubUserImg' alt='ad-pubUser' src={newAuthor.image} width={40}/> }
+                        { newAuthor.image === "" ? <img className={'ad-pubUserImg'} src='/images/public/icon_profile.png' alt=''  width={40}/> : <img className={'ad-pubUserImg'} src={newAuthor.newImage} alt=''  width={40}/> }
                         </div>
                         <Card.Body>
                             <Form className='account-details-form'>

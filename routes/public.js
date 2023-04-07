a73487e1dfd85e19eefa6af7fafca8d3913f2897
Kezziah <ayuno@ualberta.ca>
2023-04-07 10:43:10 -0600

    hmm

--- routes/public.js
+++ routes/public.js
@@ -93,7 +93,7 @@ async function getPublicLocalPosts(page, size) {
                 side: 5,
                 post: process.env.DOMAIN_NAME + "authors/" + post.author.authorId + '/posts/' + post._id,
                 id: process.env.DOMAIN_NAME + "authors/" + post.author.authorId + '/posts/' + post._id + '/comments/',
-                comments: commentHistory.comments
+                comments: commentHistory.comments !== null ? commentHistory.comments : []
             }
             delete post.commentCount;
             delete post._id;
